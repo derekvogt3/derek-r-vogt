275bb8179dee2774fe3c23cc2fe85d0566981754
Derek Vogt <derekvogt3@gmail.com>
2022-08-14 13:11:26 -0400

    experimenting with transitions

--- pages/Resume.js
+++ pages/Resume.js
@@ -7,9 +7,54 @@ export default function Resume() {
       <Navbar />
       <div className="content">
         <div className="allExperiences">
-          <div className="experienceContainer"></div>
-          <div className="experienceContainer"></div>
-          <div className="experienceContainer"></div>
+          <div className="experienceContainer">
+            <div className="left-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+            <div className="right-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+          </div>
+          <div className="experienceContainer">
+            <div className="left-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+            <div className="right-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+          </div>
+          <div className="experienceContainer">
+            <div className="left-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+            <div className="right-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+          </div>
+          <div className="experienceContainer">
+            <div className="left-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+            <div className="right-experience-container">
+              <h2>RPA Developer, Consolidation Accounting</h2>
+              <h1>Spotify</h1>
+              <h3>Jul 2020 - Aug 2021</h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
